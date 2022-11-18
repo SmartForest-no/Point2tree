cf00fe92e4ac78a8399305180474c7692aea19d2
Maciej Wielgosz <maciej.wielgosz@nibio.no>
2022-11-18 11:12:41 +0100

    run metrics script restructured

--- bash_helper_scripts/run_metrics.sh
+++ bash_helper_scripts/run_metrics.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-python nibio_postprocessing/get_instances_side_by_side.py \
---input_folder /home/nibio/mutable-outside-world/code/gitlab_fsct/instance_segmentation_classic/sample_playground/results/instance_segmented_point_clouds/ \
---output_folder /home/nibio/mutable-outside-world/code/gitlab_fsct/instance_segmentation_classic/sample_playground/results/instance_segmented_point_clouds/output \
---instance_label 'instance_nr' \
---verbose \
-# --merge
-
-python nibio_postprocessing/get_instances_side_by_side.py \
---input_folder /home/nibio/mutable-outside-world/code/gitlab_fsct/instance_segmentation_classic/sample_playground/results/instance_segmented_point_clouds_with_ground/ \
---output_folder /home/nibio/mutable-outside-world/code/gitlab_fsct/instance_segmentation_classic/sample_playground/results/instance_segmented_point_clouds_with_ground/output \
---instance_label 'instance_nr' \
---verbose \
-# --merge
-
-python nibio_postprocessing/get_instances_side_by_side.py \
---input_folder /home/nibio/mutable-outside-world/code/gitlab_fsct/instance_segmentation_classic/sample_playground/results/input_data/ \
---output_folder /home/nibio/mutable-outside-world/code/gitlab_fsct/instance_segmentation_classic/sample_playground/results/input_data/output \
---instance_label 'treeID' \
---verbose \
-# --merge
-
+python metrics/instance_segmentation_metrics_in_folder.py \
+--gt_las_folder_path /home/nibio/mutable-outside-world/code/gitlab_fsct/instance_segmentation_classic/old_sample_playground/results/input_data/ \
+--target_las_folder_path /home/nibio/mutable-outside-world/code/gitlab_fsct/instance_segmentation_classic/old_sample_playground/results/instance_segmented_point_clouds \
+--remove_ground \
+--output_folder_path /home/nibio/mutable-outside-world/code/gitlab_fsct/instance_segmentation_classic/old_sample_playground/results/metrics \
+--verbose 
