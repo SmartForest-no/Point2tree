ec2ee7aad52802dc1147518ae642d50647d69b1a
Maciej Wielgosz <maciej.wielgosz@nibio.no>
2022-12-15 11:26:39 +0100

    helper file for optimization of a single PC file

---
+++ bash_helper_scripts/get_val_corrected_after_sem_seg_single_file.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TARGET_FOLDER=/home/nibio/mutable-outside-world/code/gitlab_fsct/instance_segmentation_classic/sample_playground
+# clean the folder
+rm -rf $TARGET_FOLDER/*
+
+cp -r /home/nibio/mutable-outside-world/data/corrected/validation_after_seg_single/* $TARGET_FOLDER
+
+
