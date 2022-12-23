f0bc2a6959f2cad3a77f567c7d738a4570db5ef0
Maciej Wielgosz <maciej.wielgosz@nibio.no>
2022-12-23 21:13:19 +0100

    update in test set corrected download script

--- bash_helper_scripts/get_test_corrected.sh
+++ bash_helper_scripts/get_test_corrected.sh
@@ -6,3 +6,8 @@ rm -rf $TARGET_FOLDER/*
 
 cp /home/nibio/mutable-outside-world/data/corrected/test/*.las $TARGET_FOLDER
 
+# remove Plot71_tile_-25_-25.las from the folder
+# this file breaks the test pipeline
+rm $TARGET_FOLDER/Plot71_tile_-25_-25.las
+
+
